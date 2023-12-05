3b6e6366f9b0457738b889a4fd9133e8ad716a49
Elijah Biede <6586509+hbiede@users.noreply.github.com>
2023-12-05 10:40:34 -0700

    Update build tooling

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
+
+module.exports = {
+  resolve: {
+    fallback: {
+      crypto: require.resolve("crypto-browserify"),
+    },
+  },
+  plugins: [
+    new NodePolyfillPlugin()
+  ],
+};
